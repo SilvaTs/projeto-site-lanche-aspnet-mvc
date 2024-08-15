539412581c0f9fe9073eebe152fcd9b0ca858072
Daniel Silva <danielsilvatsi@outlook.com>
2024-08-15 20:56:37 -0300

    feat: add configurando ef core

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace LanchesMac;
+﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac;
 public class Startup
 {
     public Startup(IConfiguration configuration)
@@ -8,13 +11,14 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>(options => {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")); 
+        });
         services.AddControllersWithViews();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
@@ -24,7 +28,6 @@ public class Startup
         else
         {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
         app.UseHttpsRedirection();
