034aa949691f8d3a0a1e292ec96b39a12c54ee2f
Daniel Silva <danielsilvatsi@outlook.com>
2024-08-22 20:08:57 -0300

    feat: implementando o padrão repository categoria

---
+++ LanchesMac/Repositiries/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositiries.Interfaces;
+
+namespace LanchesMac.Repositiries;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public CategoriaRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public IEnumerable<Categoria> Categorias => _appDbContext.Categorias;
+}
