383a1473d70c917a2d19af9fd788fe5a258c40b8
Daniel Silva <danielsilvatsi@outlook.com>
2024-08-22 20:17:59 -0300

    feat: implementando o padrão repository para lanches

---
+++ LanchesMac/Repositiries/Interfaces/ILancheRepository.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositiries.Interfaces;
+
+public interface ILancheRepository
+{
+    IEnumerable<Lanche> Lanches { get; }
+    IEnumerable<Lanche> LanchesPreferido { get; }
+    Lanche GetLancheById(int lancheId);
+}
---
+++ LanchesMac/Repositiries/LancheRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositiries.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositiries;
+
+public class LancheRepository : ILancheRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public LancheRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public IEnumerable<Lanche> Lanches => _appDbContext.Lanches.Include(c => c.Categoria);
+
+    public IEnumerable<Lanche> LanchesPreferido => _appDbContext.Lanches
+                                                   .Where(l => l.IsLanchePreferido)
+                                                   .Include(c => c.Categoria);
+
+    public Lanche GetLancheById(int lancheId)
+    {
+        return _appDbContext.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+    }
+}
